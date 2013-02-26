a84e54bc3887e79d4e9ace0317d8248c0a2d9ba2
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-02-26 09:31:52 +0100

    Fixing ' bugs

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,37 +8,37 @@ class maven {
 
   file { $maven_bundle:
     ensure => present,
-    require => Exec["Fetch maven"],
+    require => Exec['Fetch maven'],
   }
 
-  exec { "Fetch maven":
-    cwd => "/tmp",
-    command => "wget $maven_url",
+  exec { 'Fetch maven':
+    cwd => '/tmp',
+    command => 'wget $maven_url',
     creates => $maven_bundle,
-    path    => ["${boxen::config::home}/homebrew/bin"],
+    path    => ['${boxen::config::home}/homebrew/bin'],
   }
 
-  file { "$maven_path":
-    require => Exec["Extract maven"],
+  file { $maven_path:
+    require => Exec['Extract maven'],
   }
 
-  exec { "Extract maven":
-    cwd     => "/usr/local",
-    command => "tar xvf $maven_bundle",
+  exec { 'Extract maven':
+    cwd     => '/usr/local',
+    command => 'tar xvf $maven_bundle',
     creates => $maven_path,
-    path    => ["/usr/bin"],
+    path    => ['/usr/bin'],
     require => File[$maven_bundle]
   }
 
-  file { "/usr/local/maven":
+  file { '/usr/local/maven':
     ensure  => link,
-    target  => "$maven_path",
+    target  => '$maven_path',
     require => File['$maven_path'],
   }
   
-  file { "${boxen::config::home}/bin/mvn": 
+  file { '${boxen::config::home}/bin/mvn': 
     ensure => link,
-    target  => "/usr/local/maven/bin/mvn",
+    target  => '/usr/local/maven/bin/mvn',
     require => File['/usr/local/maven'],
 
   }
