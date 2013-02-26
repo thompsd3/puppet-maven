08e28e24822186bc66d97874ffe5aaf2ecf9f29e
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-02-26 08:38:09 +0100

    variable paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class maven {
   $maven_url = 'http://apache.komsys.org/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz'
   $maven_bundle = "/tmp/apache-maven-3.0.5-bin.tar.gz"
+  $maven_path = /usr/local/apache-maven-3.0.5
 
   file { $maven_bundle:
     ensure => present,
@@ -11,28 +12,28 @@ class maven {
     cwd => "/tmp",
     command => "wget $maven_url",
     creates => $maven_bundle,
-    path    => ["/opt/boxen/homebrew/bin"],
+    path    => ["${boxen::config::home}/homebrew/bin"],
   }
 
-  file { "/usr/local/apache-maven-3.0.5":
+  file { "$maven_path":
     require => Exec["Extract maven"],
   }
 
   exec { "Extract maven":
     cwd     => "/usr/local",
     command => "tar xvf $maven_bundle",
-    creates => '/usr/local/apache-maven-3.0.5',
+    creates => '$maven_path',
     path    => ["/usr/bin"],
     require => File[$maven_bundle]
   }
 
   file { "/usr/local/maven":
     ensure  => link,
-    target  => "/usr/local/apache-maven-3.0.5",
-    require => File['/usr/local/apache-maven-3.0.5'],
+    target  => "$maven_path",
+    require => File['$maven_path'],
   }
   
-  file { "${boxen::config::homedir}/bin/mvn": 
+  file { "${boxen::config::home}/bin/mvn": 
     ensure => link,
     target  => "/usr/local/maven/bin/mvn",
     require => File['/usr/local/maven'],
