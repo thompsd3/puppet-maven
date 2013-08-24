13e0c88c7260eaac801ef4bb74b835b8adeffda3
Robbie <rgill@alpinedatalabs.com>
2013-08-24 13:21:34 -0700

    Do not use /tmp for less refreshes

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,9 +8,9 @@ class maven {
   }
 
   exec { 'fetch-maven':
-    cwd     => '/tmp',
+    cwd     => '/opt/maven',
     command => 'wget http://mirror.nexcess.net/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz',
-    creates => '/tmp/apache-maven-3.0.5-bin.tar.gz',
+    creates => '/opt/maven/apache-maven-3.0.5-bin.tar.gz',
     path    => ['/opt/boxen/homebrew/bin'],
     alias   => 'fetch-maven',
     require => File['opt-maven']
@@ -18,7 +18,7 @@ class maven {
 
   exec { 'extract-maven':
     cwd         => '/opt/maven',
-    command     => 'tar xvf /tmp/apache-maven-3.0.5-bin.tar.gz',
+    command     => 'tar xvf /opt/maven/apache-maven-3.0.5-bin.tar.gz',
     creates     => '/opt/maven/apache-maven-3.0.5',
     path        => ['/usr/bin'],
     refreshonly => true,
