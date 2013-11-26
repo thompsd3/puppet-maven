c51a0fb21cc1c4f89d2b663f771c9204ee687969
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-11-26 14:56:37 +0100

    upgraded maven

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,36 @@
 class maven {
 
   require wget
+  $version = 3.1.1
 
-  file { '/tmp/apache-maven-3.0.5-bin.tar.gz':
+  file { '/tmp/apache-maven-${version}-bin.tar.gz':
     ensure => present,
     require => Exec['Fetch maven'],
   }
 
   exec { 'Fetch maven':
     cwd => '/tmp',
-    command => 'wget http://apache.komsys.org/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz',
-    creates => '/tmp/apache-maven-3.0.5-bin.tar.gz',
+    command => 'wget http://apache.komsys.org/maven/maven-3/${version}/binaries/apache-maven-${version}-bin.tar.gz',
+    creates => '/tmp/apache-maven-${version}-bin.tar.gz',
     path    => ['/opt/boxen/homebrew/bin'];
   }
 
   exec { 'Extract maven':
     cwd     => '/usr/local',
-    command => 'tar xvf /tmp/apache-maven-3.0.5-bin.tar.gz',
-    creates => '/usr/local/apache-maven-3.0.5',
+    command => 'tar xvf /tmp/apache-maven-${version}-bin.tar.gz',
+    creates => '/usr/local/apache-maven-${version}',
     path    => ['/usr/bin'],
     require => Exec['Fetch maven'];
   }
 
-  file { '/usr/local/apache-maven-3.0.5':
+  file { '/usr/local/apache-maven-${version}':
     require => Exec['Extract maven'];
   }
 
   file { '/usr/local/maven':
     ensure  => link,
-    target  => '/usr/local/apache-maven-3.0.5',
-    require => File['/usr/local/apache-maven-3.0.5'];
+    target  => '/usr/local/apache-maven-${version}',
+    require => File['/usr/local/apache-maven-${version}'];
   }
   
   file { '/opt/boxen/bin/mvn': 
