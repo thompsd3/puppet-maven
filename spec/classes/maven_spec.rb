24a7a3cd0792bcdb0daca1f47f3f1d9dcab82ef6
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-11-27 11:23:27 +0100

    Fixed tests

---
+++ spec/classes/maven_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "maven" do
+  let(:facts) { default_test_facts }
+
+  it do
+    should contain_file('/usr/local/apache-maven-3.1.1').with({
+      :source  => 'puppet:///modules/java/java.sh',
+      :mode    => '0755',
+      :require => 'Package[java]'
+    })
+    should contain_file ('/usr/local/maven').with({
+      :ensure  => 'link',
+      :target  => '/usr/local/apache-maven-3.1.1',
+    })
+    should contain_file('/opt/boxen/bin/mvn').with({
+      :ensure  => 'link',
+      :target  => '/usr/local/maven/bin/mvn',
+    })
+  end
+end
