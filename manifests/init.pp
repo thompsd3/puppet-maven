9d96c53d4f8ee335b7aa2e4f66d61bac16017ce7
Donovan Thompson <donovan@nmteck.net>
2015-07-21 00:52:09 -0400

    Update maven to go in applications folder

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,27 +15,27 @@ class maven($version = '3.2.5') {
   }
 
   exec { 'Extract maven':
-    cwd     => '/usr/local',
+    cwd     => '/Applications',
     command => "tar xvf /tmp/apache-maven-${version}-bin.tar.gz",
-    creates => "/usr/local/apache-maven-${version}",
+    creates => "/Applications/apache-maven-${version}",
     path    => ['/usr/bin'],
     require => Exec['Fetch maven'];
   }
 
-  file { "/usr/local/apache-maven-${version}":
+  file { "/Applications/apache-maven-${version}":
     require => Exec['Extract maven'];
   }
 
-  file { '/usr/local/maven':
+  file { '/Applications/maven':
     ensure  => link,
-    target  => "/usr/local/apache-maven-${version}",
+    target  => "/Applications/apache-maven-${version}",
     require => File["/usr/local/apache-maven-${version}"];
   }
   
   file { '/opt/boxen/bin/mvn': 
     ensure => link,
-    target  => '/usr/local/maven/bin/mvn',
-    require => File['/usr/local/maven'];
+    target  => '/Applications/maven/bin/mvn',
+    require => File['/Applications/maven'];
   }
 
 }
