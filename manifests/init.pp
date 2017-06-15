ed2813966d28e6bd5c59e446869c36203d061e20
thompsd3 <dthompson.nmteck@gmail.com>
2017-06-15 09:43:54 -0400

    Create init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,8 +9,8 @@ class maven {
 
   exec { 'fetch-maven':
     cwd     => '/opt/maven',
-    command => 'wget http://mirror.nexcess.net/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz',
-    creates => '/opt/maven/apache-maven-3.0.5-bin.tar.gz',
+    command => 'wget http://mirror.nexcess.net/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz',
+    creates => '/opt/maven/apache-maven-3.5.0-bin.tar.gz',
     path    => ['/usr/local/bin/'],
     alias   => 'fetch-maven',
     require => File['opt-maven']
@@ -18,8 +18,8 @@ class maven {
 
   exec { 'extract-maven':
     cwd         => '/opt/maven',
-    command     => 'tar xvf /opt/maven/apache-maven-3.0.5-bin.tar.gz',
-    creates     => '/opt/maven/apache-maven-3.0.5',
+    command     => 'tar xvf /opt/maven/apache-maven-3.5.0-bin.tar.gz',
+    creates     => '/opt/maven/apache-maven-3.5.0',
     path        => ['/usr/bin'],
     refreshonly => true,
     subscribe   => Exec['fetch-maven'],
@@ -28,7 +28,7 @@ class maven {
   
   file { '/opt/boxen/bin/mvn': 
     ensure  => link,
-    target  => '/opt/maven/apache-maven-3.0.5/bin/mvn',
+    target  => '/opt/maven/apache-maven-3.5.0/bin/mvn',
     require => Exec['extract-maven']
   }
 
